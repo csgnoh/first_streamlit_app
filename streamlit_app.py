9400780943210bb990c5c5b0e4c9a466e96b21a2
csgnoh <gitgcs@gmail.com>
2022-08-24 16:27:03 +0800

    Restructured code into IF-ELSE block

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,22 @@ streamlit.dataframe(fruits_to_show)
 
 # display the response code from fruityvice
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# restructured code in if else structure with error handling
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+    if not fruit_choice:
+        streamlit.error('Please select a fruit to get information.')
+    else:
+        #import requests
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        # take the json version of the response and normalize it
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        # display the normalized json output instead of raw output
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
-# take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display the normalized json output instead of raw output
-streamlit.dataframe(fruityvice_normalized)
 
 # put a stop here
 streamlit.stop()
