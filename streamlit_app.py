0ce95d734ee4220b2339a678a74c66bafef26bb5
csgnoh <gitgcs@gmail.com>
2022-08-24 16:29:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 # restructured code in if else structure with error handling
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error('Please select a fruit to get information.')
     else:
